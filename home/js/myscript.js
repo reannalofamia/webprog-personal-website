4392ae75204aaf5d932d7654c57e040291f2bf6f
reannalofamia <rplofamia@student.apc.edu.ph>
2024-12-16 18:33:05 +0800

    added about me button

--- home/js/myscript.js
+++ home/js/myscript.js
@@ -1,3 +1,13 @@
 function myFunction() {
-    document.getElementById("demo").innerHTML = "Paragraph changed.";
-  }
+  document.getElementById("demo").innerHTML = "Paragraph changed.";
+}
+
+function toggleAboutMe() {
+const aboutMeElement = document.getElementById("about-me");
+
+if (aboutMeElement.style.display === "none") {
+    aboutMeElement.style.display = "block";
+} else {
+    aboutMeElement.style.display = "none";
+}
+}  
